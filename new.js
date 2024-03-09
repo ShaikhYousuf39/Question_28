46eb7f858104cd60db134912882490e6cb47c8f0
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:21:31 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,21 @@
+"use strict";
+let personAge = 40;
+if (personAge < 2) {
+    console.log("person is baby");
+}
+else if (personAge < 4) {
+    console.log('person is toddler');
+}
+else if (personAge < 13) {
+    console.log("person is a kid");
+}
+else if (personAge < 20) {
+    console.log("person is a teenager");
+}
+else if (personAge < 65) {
+    console.log("person is an adult");
+}
+else {
+    (personAge > 65);
+    console.log("person is an elder");
+}
